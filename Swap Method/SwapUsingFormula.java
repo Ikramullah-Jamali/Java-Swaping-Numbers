a380b602a99d1616a092fa49dce95f37adfb3370
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-08-19 14:25:59 +0500

    Create SwapUsingFormula.java

---
+++ Swap Method/SwapUsingFormula.java
@@ -0,0 +1,16 @@
+public class SwapUsingFormula 
+{
+    static void SwapByMathFormula()
+    {
+        int a=100,b=200;
+        System.out.println("the value a= "+a+" and b= "+b);
+        a=(a+b)-(b=a);
+        System.out.println(" .... After Swapping ....");
+        System.out.println("the value a= "+a+" and b= "+b);
+
+    }
+    public static void main(String [] args)
+    {
+        SwapByMathFormula();
+    }
+}
